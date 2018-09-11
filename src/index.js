2e226bd3f9d6324b6d3ab4c20e92809668cfc112
Shashank Adavi <shashank.ap28@gmail.com>
2018-09-10 22:22:35 -0400

    index.js updates

--- src/index.js
+++ src/index.js
@@ -12,30 +12,50 @@ const handlers = {
     'LaunchRequest': function () {
         var self = this;
         this.attributes['Category']='';
-        this.attributes['Amount'] = '';
+      //  this.attributes['Amount'] = '';
 
-        let jeopardy = JSON.parse(board);
+      //  let jeopardy = JSON.parse(board);
 
-        this.attributes['jeopardy'] = jeopardy;
+      //  this.attributes['jeopardy'] = board;
 
-        let speechOutput = "Welcome to ICC Jeopardy! Please select a category";
-        self.response.speak(speechOutput).listen(speechOutput);
+       let speechOutput = "Welcome to ICC Jeopardy! Please select a category and amount";
+       self.response.speak(speechOutput).listen(speechOutput);
+      console.log("THIS.EVENT = " + JSON.stringify(this.event));
+        self.emit(':responseReady');
 
-        this.emit('ICCJeopardyIntent');
     },
 
     'ICCJeopardyIntent': function () {
 
-        let category = this.event.request.intent.slots['Category'].value.toString().toLowerCase();
+        console.log("THIS.EVENT = " + JSON.stringify(this.event));
+        //console.log("SLOT IS :" + JSON.stringify(this.event.request.intent.slots.Category));
+
+        // let category = this.event.request.intent.slots.Category.value.toString().toLowerCase();
+
+        console.log("Category is :"+ this.event.request.intent.slots.Category.resolutions.resolutionsPerAuthority[0].values[0].value.name);
+
+        let category = this.event.request.intent.slots.Category.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+
+        this.attributes['Category'] = category;
+        this.attributes['Amount'] ='';
+
 
         let existsCategory = checkCategoryExists(category);
 
         if(existsCategory){
 
-          let speechAmount = "Great! Please select an amount";
-          this.response.speak(speechAmount).listen(speechAmount);
+         let speechAmount = "Great! Please select an amount";
+         this.response.speak(speechAmount).listen(speechAmount);
+         this.emit('CaptureAmountIntent');
 
-          let amount = this.event.request.intent.slots['Amount'].value.toString().toLowerCase();
+         /*console.log("THIS.EVENT = " + JSON.stringify(this.event));
+         console.log("SLOTS ARE:" + JSON.stringify(this.event.request.intent.slots));
+
+         //let amount = this.event.request.intent.slots.Amount.value.toString().toLowerCase();
+
+         console.log("Amount is :"+ this.event.request.intent.slots.Amount.resolutions.resolutionsPerAuthority[0].values[0].value.name)
+
+         let amount =  this.event.request.intent.slots.Amount.resolutions.resolutionsPerAuthority[0].values[0].value.name;
 
           let existsAmount = checkAmountExists(amount);
 
@@ -45,13 +65,13 @@ const handlers = {
 
               this.response.speak(jeopardyAnswer).listen(jeopardyAnswer);
 
-              let name = this.event.request.intent.slots['Women'].value.toString().toLowerCase();
+              // let name = this.event.request.intent.slots['Women'].value.toString().toLowerCase();
 
-<<<<<<< HEAD
-              let isCorrectQuestion = verifyJeopardyQuestion(name,category,amount);
-=======
+              console.log("Name of women is :"+ this.event.request.intent.slots.Women.resolutions.resolutionsPerAuthority[0].values[0].value.name)
+
+              let name = this.event.request.intent.slots.Women.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+
               let isCorrectQuestion = verifyJeopardyQuestion(name, category, amount);
->>>>>>> f805ed6a3239c5f7c5a0211d18926967df847739
 
                   if(isCorrectQuestion){
                     this.response.speak('Correct Answer');
@@ -65,15 +85,45 @@ const handlers = {
               let errorAmount = 'Sorry! Incorrect amount';
 
               this.response.speak(errorAmount);
-              this.emit('ICCJeopardyIntent');
-            }
+              // this.emit('ICCJeopardyIntent');
+            }*/
         }
         else{
 
+          let errorCategory = 'Sorry! Incorrect category!Please start ICC jeopardy again';
+          this.response.speak(errorAmount);
+          this.emit('ICCJeopardyIntent');
         }
 
         this.emit(':responseReady')
-    }
+    },
+    'CaptureAmountIntent': function (){
+      console.log("AMOUNT INTENT THIS.EVENT = " + JSON.stringify(this.event));
+      console.log("AMOUNT INTENT SLOT is:" + JSON.stringify(this.event.request.intent.slots));
+
+      //let amount = this.event.request.intent.slots.Amount.value.toString().toLowerCase();
+
+      console.log("Amount is :"+ this.event.request.intent.slots.Amount.resolutions.resolutionsPerAuthority[0].values[0].value.name)
+
+      let amount =  this.event.request.intent.slots.Amount.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+
+       let existsAmount = checkAmountExists(amount);
+
+         if(existsAmount){
+
+           let jeopardyAnswer = tellJeopardyAnswer(category,amount);
+
+           this.response.speak(jeopardyAnswer).listen(jeopardyAnswer);
+         }
+    },
+    'SessionEndedRequest' : function() {
+
+   },
+   'Unhandled': function (){
+       this.response.speak('I did not understand that!Please start again!');
+       this.emit(':responseReady')
+   }
+
 };
 
 function checkCategoryExists(category){
@@ -104,21 +154,21 @@ function tellJeopardyAnswer(category,amount){
 }
 
 function verifyJeopardyQuestion(question, category, amount){
-<<<<<<< HEAD
-    question = "who is" + question.toString().toLowerCase();
+    question = "who is " + question.toString().toLowerCase();
 
-    let boardQuestion = board[category][amount].boardQuestion.toLowerCase();
+    let boardQuestion = board[category][amount].question.toLowerCase();
 
     if (question == boardQuestion) {
         return true;
     }
     return false;
-=======
-	let question = "who is" + question.toString().toLowerCase();
+
+/*	let question = "who is" + question.toString().toLowerCase();
 	let answer = board[category][amount].answer.toLowerCase();
 	if (question == answer) {
 		return true;
 	}
 	return false;
->>>>>>> f805ed6a3239c5f7c5a0211d18926967df847739
+  */
+
 }
